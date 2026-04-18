a1cc15d021d76c488cd2e099b61696011d3fce97
kiwiliwwi9-afk <kiwiliwwi9@gmail.com>
2026-04-18 21:54:32 +0300

    Create sw.js

---
+++ static/sw.js
@@ -0,0 +1,20 @@
+const CACHE_NAME = 'go-world-v1';
+const urlsToCache = [
+  '/',
+  '/static/style.css',
+  '/static/icon-192.png'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
+});
