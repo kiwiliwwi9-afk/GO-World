a5c5a4144317766b015330b498c3823bf8ba76fc
kiwiliwwi9-afk <kiwiliwwi9@gmail.com>
2026-04-18 11:40:09 +0300

    Update app.py

--- app.py
+++ app.py
@@ -163,4 +163,5 @@ def unfollow(username):
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 10000))
+    app.run(host='0.0.0.0', port=port, debug=False)
